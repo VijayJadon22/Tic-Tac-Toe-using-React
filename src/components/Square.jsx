0f372895c0976ab82714b1ea75ebd3e061046787
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2024-12-24 19:57:28 +0530

    Implement a basic tic tac toe game using react

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
-const Square = () => {
-    return (
-        <div
-            style={{
-                border: "1px solid black",
-                borderCollapse:"collapse",
-                height: "100px",
-                width: "100%",
-                display:"flex",
-                justifyContent:"center",
-                alignItems: "center",
-                fontSize:"25px"
-            }}
-            className="square">
-            <h5>O</h5>
-        </div>
-    )
+const Square = ({ value, onClick }) => {
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        border: "2px solid grey",
+        borderCollapse: "collapse",
+        height: "100px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+          fontSize: "25px",
+        userSelect:"none"
+      }}
+      className="square"
+    >
+      <h5>{value}</h5>
+    </div>
+  );
 };
 
 export default Square;
